6a1e3ccb3bd6617efdf628801f61865d838a5de9
steven <stevenle2011@gmail.com>
2017-08-05 10:02:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-redux
+
+React Redux boilerplate.
+
+
+npm update --save
+
+npm update --save-dev
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname,'dist')
+  },
+  devServer:{
+    publicPath:'/',
+    contentBase:'./public',
+  	inline:true,
+  	port:8080,
+   
+  },
+
+  module: {
+  loaders: [
+    { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader", 
+		
+    query:{presets:['react','es2015']}
+	}
+  ]
+}
+};
